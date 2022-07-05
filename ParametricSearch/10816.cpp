a9b942a3a37bce9f701182e964134ac9e203e357
이도훈 <d.lee@digipen.edu>
2022-07-05 11:49:10 +0000

    2022/07/05 백준 이분탐색 문제

---
+++ ParametricSearch/10816.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int n, m;
+std::vector<int> cards;
+
+int BSLower(int num){
+	int lo, hi, mid;
+	lo = 0;
+	hi = cards.size() - 1;
+	while(lo <= hi){
+		mid = (lo + hi) / 2;
+		if(cards[mid] < num){
+			lo = mid + 1;
+		}else{
+			hi = mid - 1;
+		}
+	}
+	return lo;
+}
+
+int BSUpper(int num){
+	int lo, hi, mid;
+	lo = 0;
+	hi = cards.size() - 1;
+	while(lo <= hi){
+		mid = (lo + hi) / 2;
+		if(cards[mid] <= num){
+			lo = mid + 1;
+		}else{
+			hi = mid - 1;
+		}
+	}
+	return hi;
+}
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	
+	int temp;
+	std::cin >> n;
+	for(int i = 0; i < n; i++){
+		std::cin >> temp;
+		cards.push_back(temp);
+	}
+	std::sort(cards.begin(), cards.end());
+	std::cin >> m;
+	for(int i = 0; i < m; i++){
+		std::cin >> temp;
+		std::cout << BSUpper(temp) - BSLower(temp) + 1 << " ";
+	}
+}
